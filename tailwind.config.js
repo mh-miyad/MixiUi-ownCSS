c1a7864782df40b0e871c2f26c8223c927625e5c
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-03-04 10:35:28 +0600

    understanding of different of inset

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   darkMode: "class",
   content: [
-    "*.{html,js}",
-    "./Components/**/*.{js,jsx,ts,tsx}",
-    "./Pages/**/*.{js,jsx,ts,tsx}",
+    "*",
     "./node_modules/flowbite/**/*.js",
     "./node_modules/tw-elements/dist/js/**/*.js",
   ],
